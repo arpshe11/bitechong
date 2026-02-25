e954fb41368beef5c12e6443b77948ed8cf77b22
arpshe11 <arpshe11@example.com>
2026-02-25 12:34:51 +0800

    chore: 顶部导航使用bitechong图标

--- src/App.tsx
+++ src/App.tsx
@@ -144,11 +144,7 @@ function App() {
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center">
               <div className="flex-shrink-0">
-                <div className="w-8 h-8 bg-blue-500 rounded-lg flex items-center justify-center">
-                  <svg className="w-5 h-5 text-white" fill="currentColor" viewBox="0 0 20 20">
-                    <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
-                  </svg>
-                </div>
+                <img src="/bitechong.svg" alt="logo" className="w-8 h-8" />
               </div>
               <h1 className="ml-3 text-xl font-bold text-gray-900">
                 ICO图标制作工具
